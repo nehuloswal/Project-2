c70a262056f8f647894f28d593c97c8ce42061b5
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2019-10-26 15:35:28 -0400

    conv control start

--- Part1/part1.sv
+++ Part1/part1.sv
@@ -12,12 +12,12 @@ module memory(clk, data_in, data_out, addr, wr_en);
 	end
 endmodule
 
-module memory_control_xf(clk, reset, s_valid_x, s_ready_x, m_addr_x, ready_write, conv_done);
+module memory_control_xf(clk, reset, s_valid_x, s_ready_x, m_addr_x, ready_write, conv_done, read_done);
 	parameter LOGSIZE = 6, SIZE = 8;
 	input clk, reset, s_valid_x, ready;
 	output s_ready_x, wen;
 	output [LOGSIZE - 1:0] m_addr_x;
-	logic count, conv_done;
+	logic count, conv_done, read_done;
 
 	always_ff @(posedge clk) begin
 		if (reset) begin
@@ -25,6 +25,7 @@ module memory_control_xf(clk, reset, s_valid_x, s_ready_x, m_addr_x, ready_write
 			m_addr_x <= 0;
 			ready_write <= 0;
 			m_addr_x <= 0;
+			read_done <= 0;
 		end 
 		else begin
 			if(s_ready_x == 1 && s_valid_x == 1)
@@ -36,8 +37,10 @@ module memory_control_xf(clk, reset, s_valid_x, s_ready_x, m_addr_x, ready_write
 			m_addr_x <= m_addr_x + 1;
 		if (m_addr_x < (SIZE - 1)) 
 			s_ready_x <= 1;
-		else 
+		else begin
 			s_ready_x <= 0;
+			read_done <= 1;
+		end
 		if (conv_done == 1) begin
 			s_ready_x <= 1;
 			m_addr_x <= 0;
@@ -45,7 +48,18 @@ module memory_control_xf(clk, reset, s_valid_x, s_ready_x, m_addr_x, ready_write
 	end
 endmodule
 
-module conv_control();
+module conv_control(reset, clk, m_addr_read_x, m_addr_read_f, conv_done, read_done_x, read_done_f, m_valid_y, m_ready_y);
+	input reset, clk, read_done_x, read_done_f, m_ready_y;
+	output [7 : 0] m_addr_read_x;
+	output [3 : 0] m_addr_read_f,;
+	output conv_done, m_valid_y;
+	if (reset == 1) begin
+		m_addr_read_f <= 0;
+		m_addr_read_x <= 0;
+		conv_done <= 0;
+		m_valid_y <= 0;
+
+
 endmodule
 
 module conv_8_4(clk, reset, s_data_in_x, s_valid_x, s_ready_x, s_data_in_f, s_valid_f, s_ready_f, m_data_out_y, m_valid_y, m_ready_y);
