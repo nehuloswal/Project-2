aadeb6149965d04c0a8bc315ba9b14b3a3459d98
Hrushikesh Patil 2294 <patilh@cadsrv1.ece.stonybrook.edu>
2019-10-26 23:14:42 -0400

    error fix

--- Part1/part1.sv
+++ Part1/part1.sv
@@ -15,8 +15,8 @@ endmodule
 module memory_control_xf(clk, reset, s_valid_x, s_ready_x, m_addr_x, ready_write, conv_done, read_done);
 	parameter LOGSIZE = 6, SIZE = 8;
 	input clk, reset, s_valid_x;
-	output s_ready_x, read_done, ready_write, conv_done;
-	output [LOGSIZE - 1:0] m_addr_x;
+	output logic s_ready_x, read_done, ready_write, conv_done;
+	output logic [LOGSIZE - 1:0] m_addr_x;
 
 	always_ff @(posedge clk) begin
 		if (reset) begin
@@ -49,49 +49,51 @@ endmodule
 
 module conv_control(reset, clk, m_addr_read_x, m_addr_read_f, conv_done, read_done_x, read_done_f, m_valid_y, m_ready_y, en_acc, clr_acc);
 	input reset, clk, read_done_x, read_done_f, m_ready_y;
-	output [2:0] m_addr_read_x;
-	output [1:0] m_addr_read_f;
-	output conv_done, m_valid_y, en_acc, clr_acc;
+	output logic [2:0] m_addr_read_x;
+	output logic [1:0] m_addr_read_f;
+	output logic conv_done, m_valid_y, en_acc, clr_acc;
 	logic hold_state;
 	logic [2:0] number_x;
 
-	if (reset == 1) begin
-		m_addr_read_f <= 0;
-		m_addr_read_x <= 0;
-		conv_done <= 0;
-		m_valid_y <= 0;
-		en_acc <= 0;
-		clr_acc <= 1;
-		number_x <= 1;
-	end
-	else begin 
-		if (read_done_x && read_done_f && hold_state == 0) begin
-			en_acc <= 1;
-			clr_acc <= 0;
-			m_addr_read_x <= m_addr_read_x + 1;
-			m_addr_read_f <= m_addr_read_f + 1;
-		end
-		if ((m_addr_read_f == 3) && (hold_state == 0)) begin
-			m_addr_read_x <= number_x;
-			number_x <= number_x + 1;
+	always_ff @(posedge clk) begin
+		if (reset == 1) begin
 			m_addr_read_f <= 0;
-			m_valid_y <= 1;
-		end
-		if ((number_x == 5) && (m_addr_read_f == 3)) begin
-			conv_done <= 1;
-		end
-		if ((m_valid_y == 1) && (m_ready_y == 0)) begin
-			hold_state <= 1;
-			en_acc <= 0;
-		end
-		else begin
-			hold_state <= 0;
-			en_acc <= 1;
-		end
-		if ((m_valid_y == 1) && (m_ready_y == 1)) begin
-			m_valid_y <= 0;
+			m_addr_read_x <= 0;
 			conv_done <= 0;
+			m_valid_y <= 0;
+			en_acc <= 0;
 			clr_acc <= 1;
+			number_x <= 1;
+		end
+		else begin 
+			if (read_done_x && read_done_f && hold_state == 0) begin
+				en_acc <= 1;
+				clr_acc <= 0;
+				m_addr_read_x <= m_addr_read_x + 1;
+				m_addr_read_f <= m_addr_read_f + 1;
+			end
+			if ((m_addr_read_f == 3) && (hold_state == 0)) begin
+				m_addr_read_x <= number_x;
+				number_x <= number_x + 1;
+				m_addr_read_f <= 0;
+				m_valid_y <= 1;
+			end
+			if ((number_x == 5) && (m_addr_read_f == 3)) begin
+				conv_done <= 1;
+			end
+			if ((m_valid_y == 1) && (m_ready_y == 0)) begin
+				hold_state <= 1;
+				en_acc <= 0;
+			end
+			else begin
+				hold_state <= 0;
+				en_acc <= 1;
+			end
+			if ((m_valid_y == 1) && (m_ready_y == 1)) begin
+				m_valid_y <= 0;
+				conv_done <= 0;
+				clr_acc <= 1;
+			end
 		end
 	end
 endmodule
@@ -100,30 +102,35 @@ module convolutioner(clk, reset, m_addr_read_x, m_addr_read_f, m_data_out_y, en_
 	input clk, reset, en_acc, clr_acc;
 	input [2:0] m_addr_read_x;
 	input [1:0] m_addr_read_f;
-	output [17:0] m_data_out_y;
+	output logic [17:0] m_data_out_y;
 	logic [7:0] m_data_x;
 	logic [3:0] m_data_f;
 	logic [15:0] w_mult_op;
 	logic [17:0] w_addr_op, tmp_out;
 
 	always_comb begin
-		if (en_acc) begin
+		if (reset) begin
+			w_addr_op = 0;
+			w_mult_op = 0;
+		end
+		else if (clr_acc) begin
+			w_addr_op = 0;
+			w_mult_op = 0;
+		end
+		else if (en_acc) begin
 			w_mult_op = m_data_x * m_data_f;
 			w_addr_op = w_mult_op + m_data_out_y;
 		end
+	end
 
 	always_ff @(posedge clk) begin
 		if (reset) begin
 			m_data_out_y <= 0;
 			tmp_out <= 0;
-			w_addr_op <= 0;
-			w_mult_op <= 0;
 		end
 		else if (clr_acc) begin
 			m_data_out_y <= 0;
 			tmp_out <= 0;
-			w_addr_op <= 0;
-			w_mult_op <= 0;
 		end
 		else if (en_acc)
 			m_data_out_y <= w_addr_op;
@@ -154,3 +161,4 @@ module conv_8_4(clk, reset, s_data_in_x, s_valid_x, s_ready_x, s_data_in_f, s_va
 	conv_control cc(.reset(reset), .clk(clk), .m_addr_read_x(w_to_addrx), .m_addr_read_f(w_to_addrf), .conv_done(w_conv_done), .read_done_x(w_read_done_x), .read_done_f(w_read_done_f), .m_valid_y(m_valid_y), .m_ready_y(m_ready_y), .en_acc(e_acc), .clr_acc(c_acc));
 
 	convolutioner conv(.clk(clk), .reset(reset), .m_addr_read_x(w_to_addrx), .m_addr_read_f(w_to_addrf), .m_data_out_y(m_data_out_y), .en_acc(e_acc), .clr_acc(c_acc));
+endmodule
